e32d6ca31635c0194242e7a1eb5ad4af3f23a8ba
unawares <unawares15@gmail.com>
2019-09-06 16:55:52 +0600

    Fix

--- multibrand/settings.py
+++ multibrand/settings.py
@@ -16,7 +16,7 @@ from docutils.core import publish_parts
 from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
